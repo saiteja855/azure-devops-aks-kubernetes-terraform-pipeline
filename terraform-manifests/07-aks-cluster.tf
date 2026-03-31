6733794b6d4436fa23dd4a98c36d0ff08ca0792b
saiteja855 <69745829+saiteja855@users.noreply.github.com>
2026-03-31 12:41:14 +0530

    Update 07-aks-cluster.tf

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -7,32 +7,32 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   node_resource_group = "${azurerm_resource_group.aks_rg.name}-nrg"
 
 
-  default_node_pool {
-    name       = "systempool"
-    vm_size    = "Standard_DS2_v2"
-    orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
-    #availability_zones   = [1, 2, 3]
-    # Added June2023
-    zones = [1, 2, 3]
-    #enable_auto_scaling  = true # COMMENTED OCT2024
-    auto_scaling_enabled = true  # ADDED OCT2024
-    max_count            = 3
-    min_count            = 1
-    os_disk_size_gb      = 30
-    type           = "VirtualMachineScaleSets"
-    node_labels = {
-      "nodepool-type" = "system"
-      "environment"   = var.environment
-      "nodepoolos"    = "linux"
-      "app"           = "system-apps"
-    }
-    tags = {
-      "nodepool-type" = "system"
-      "environment"   = var.environment
-      "nodepoolos"    = "linux"
-      "app"           = "system-apps"
-    }    
+ default_node_pool {
+  name       = "systempool"
+  vm_size    = "Standard_D2ls_v6"
+  orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
+ 
+  auto_scaling_enabled = true
+  max_count            = 1
+  min_count            = 1
+  os_disk_size_gb      = 30
+ 
+  type = "VirtualMachineScaleSets"
+ 
+  node_labels = {
+    "nodepool-type" = "system"
+    "environment"   = var.environment
+    "nodepoolos"    = "linux"
+    "app"           = "system-apps"
   }
+ 
+  tags = {
+    "nodepool-type" = "system"
+    "environment"   = var.environment
+    "nodepoolos"    = "linux"
+    "app"           = "system-apps"
+  }
+}
 
 # Identity (System Assigned or Service Principal)
   identity { type = "SystemAssigned" }
